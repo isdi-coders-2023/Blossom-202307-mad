9c5ae2f51622068cf6f7f7a05eb07b41d4f291f0
Sergio G. Girona <sergioglez_8@hotmail.com>
2023-08-14 17:18:52 +0200

    Add apirepository class and test 2 of them

---
+++ src/components/api-characters-repository.test.ts
@@ -0,0 +1,120 @@
+import { ApiSimpsonsRepository } from './api-characters-repository';
+
+describe('Given ApiCharactersRepository class ', () => {
+  describe('When we instantiate it', () => {
+    const repository = new ApiSimpsonsRepository('');
+
+    test('Then the method getAll should be used', () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: true,
+        json: jest.fn().mockResolvedValue('Test'),
+      });
+      repository.getAll();
+      expect(global.fetch).toHaveBeenCalled();
+    });
+
+    test('Then the method getAll should give error', () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: false,
+        json: jest.fn().mockResolvedValue('error'),
+      });
+
+      expect(repository.getAll()).rejects.toThrow();
+    });
+
+    test('Then the method update should be used', () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: true,
+        json: jest.fn().mockResolvedValue('Test'),
+      });
+      const characterTest = {
+        Estado: true,
+        Genero: 'Masculino',
+        Historia: 'TEST 01',
+        Imagen: 'img',
+        Nombre: 'NAME',
+        Ocupacion: 'developer',
+        updatedAt: 'test_update',
+        _id: 'testid',
+      };
+      const characterUpdatedTest = {
+        Estado: true,
+        Genero: 'Femenino',
+        Historia: 'TEST 01.2',
+        Imagen: 'img',
+        Nombre: 'NAME',
+        Ocupacion: 'developer',
+        updatedAt: 'test_update',
+        _id: 'testid',
+      };
+      repository.update(characterTest._id, characterUpdatedTest);
+      expect(global.fetch).toHaveBeenCalled();
+    });
+    test('Then the method update should give error', () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: false,
+        json: jest.fn().mockResolvedValue('error'),
+      });
+      const characterTest = {
+        Estado: true,
+        Genero: 'Masculino',
+        Historia: 'TEST 01',
+        Imagen: 'img',
+        Nombre: 'NAME',
+        Ocupacion: 'developer',
+        updatedAt: 'test_update',
+        _id: 'testid',
+      };
+      const characterUpdatedTest = {
+        Estado: true,
+        Genero: 'Femenino',
+        Historia: 'TEST 01.2',
+        Imagen: 'img',
+        Nombre: 'NAME',
+        Ocupacion: 'developer',
+        updatedAt: 'test_update',
+        _id: 'testid',
+      };
+      expect(
+        repository.update(characterTest._id, characterUpdatedTest)
+      ).rejects.toThrow();
+    });
+
+    test('Then the method create should be used', () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: true,
+        json: jest.fn().mockResolvedValue('Test'),
+      });
+      const newCharacterTest = {
+        Estado: true,
+        Genero: 'Femenino',
+        Historia: 'TEST 01.2',
+        Imagen: 'img',
+        Nombre: 'NAME',
+        Ocupacion: 'developer',
+        updatedAt: 'test_update',
+        _id: 'testid',
+      };
+      repository.create(newCharacterTest);
+      expect(global.fetch).toHaveBeenCalled();
+    });
+    test('Then the method create should give error', () => {
+      global.fetch = jest.fn().mockResolvedValueOnce({
+        ok: false,
+        json: jest.fn().mockResolvedValue('error'),
+      });
+      const newCharacterTest = {
+        Estado: true,
+        Genero: 'Masculino',
+        Historia: 'TEST 01',
+        Imagen: 'img',
+        Nombre: 'NAME',
+        Ocupacion: 'developer',
+        updatedAt: 'test_update',
+        _id: 'testid',
+      };
+
+      expect(repository.create(newCharacterTest)).rejects.toThrow();
+    });
+  });
+});
---
+++ src/components/api-characters-repository.tsx
@@ -0,0 +1,63 @@
+import { Character } from '../model/character';
+import { Repository } from './api-repository/repository';
+
+export class ApiSimpsonsRepository implements Repository<Character[]> {
+  urlBase: string;
+  constructor(urlBase: string) {
+    this.urlBase = urlBase;
+  }
+
+  async getAll(): Promise<Character[]> {
+    const request = await fetch(this.urlBase);
+    if (!request.ok) {
+      throw new Error(
+        `Error ${request.status}: ${request.statusText}. Try again.`
+      );
+    }
+    const data = await request.json();
+    return data;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async create(_item: Character) {
+    const request = await fetch(this.urlBase, {
+      method: 'POST',
+      body: 'JSON.stringify',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!request.ok)
+      throw new Error(
+        `Error ${request.status}: ${request.statusText}. Try again.`
+      );
+    const data = await request.json();
+    return data;
+  }
+  async update(_id: string, item: Partial<Character>): Promise<Character> {
+    const url = this.urlBase + '/' + _id;
+    const request = await fetch(url, {
+      method: 'PATCH',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!request.ok)
+      throw new Error(
+        `Error ${request.status}: ${request.statusText}. Try again.`
+      );
+    const data = await request.json();
+    return data;
+  }
+
+  async delete(_id: string): Promise<void> {
+    const url = this.urlBase + '/' + _id; //aquí no sé cómo coger el elemento para borrarlo, ya que no tiene un id específico
+    const request = await fetch(url, {
+      method: 'DELETE',
+    });
+
+    if (!request.ok)
+      throw new Error(`Error ${request.status}: ${request.statusText}`);
+  }
+}
