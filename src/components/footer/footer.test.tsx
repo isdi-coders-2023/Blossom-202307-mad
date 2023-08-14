ba4f5c5c1a29f54955cc484239b1a709248ac396
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-14 17:27:23 +0200

    Update footer test

--- src/components/footer/footer.test.tsx
+++ src/components/footer/footer.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import React from 'react';
-import { Footer } from './footer/footer';
+import { Footer } from './footer';
 
 describe('Given the component Footer', () => {
   describe('When we render it', () => {
