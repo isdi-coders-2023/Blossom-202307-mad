df6f8888fe7ec66d83ad9488ddd1d66fce4e3390
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-14 17:21:10 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,6 @@ export default {
   testPathIgnorePatterns: ['dist'],
   resolver: 'jest-ts-webcompat-resolver',
   moduleNameMapper: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    '.(css|scss)$': 'identity-obj-proxy',
-=======
     '\\.(css|scss)$': 'identity-obj-proxy',
->>>>>>> main
-=======
-    '\\.(css|scss)$': 'identity-obj-proxy',
->>>>>>> 57227f475b8377d9c9670a23c59d2e60516184d1
   },
 };
