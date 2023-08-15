85e9f13320ac9036771a220142e45a5011633a58
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-15 12:58:22 +0200

    done Pag-error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
+import { PagError } from './components/pag-error/pag-error';
 function App() {
   return (
     <div>
       <Header></Header>
+      <PagError />
       <Footer />
     </div>
   );
