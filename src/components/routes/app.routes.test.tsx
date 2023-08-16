e281674e13b1ca2336824bee9d295e9a744cbc63
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-16 10:05:39 +0200

    done route test

--- src/components/routes/app.routes.test.tsx
+++ src/components/routes/app.routes.test.tsx
@@ -2,13 +2,12 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { MemoryRouter as Router } from 'react-router-dom';
-import PagError from '../../pages/error.pages/pag-error';
 import { AppRouter } from './app.routes';
 
 describe('Given the app routes component', () => {
   describe('When we render error Router', () => {
-    const mockedPagError = jest.fn().mockReturnValue(<span>/¡ HORROR !/</span>);
-    jest.mock('../../pages/error.pages/pag-error', () => PagError);
+    const mockedPagError = jest.fn().mockReturnValue(<span>¡ HORROR !</span>);
+    jest.mock('../../pages/error.pages/pag-error', () => mockedPagError);
     //'../pag-error/pag-error'
 
     let element: HTMLElement;
@@ -21,7 +20,7 @@ describe('Given the app routes component', () => {
         );
       });
 
-      element = screen.getByText('¡ HORROR !');
+      element = screen.getByText(/horror/i);
     });
 
     test('Then it should be in the document', () => {
