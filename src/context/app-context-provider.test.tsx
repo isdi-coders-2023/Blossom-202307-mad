9744baf50b51860d17c256dbc28346e411ed2434
Jeff <jeffersonasto95@gmail.com>
2023-08-20 16:38:34 +0200

    Add test of chracters

---
+++ src/context/app-context-provider.test.tsx
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { WebContextProvider } from './app-context-provider';
+
+describe('Given a PlanetContextProvider', () => {
+  test('Should render children', () => {
+    render(
+      <WebContextProvider>
+        <h1>Test</h1>
+      </WebContextProvider>
+    );
+    const firstChildren = screen.getByText('Test');
+
+    expect(firstChildren).toBeInTheDocument();
+  });
+});
